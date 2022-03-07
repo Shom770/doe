ef47e12d777260b00523657b7fa5935972a1b483
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-03-07 10:01:26 -0500

    textual attempt

--- doe.py
+++ doe.py
@@ -1,4 +1,8 @@
 import click
+from rich.panel import Panel
+from textual.app import App
+from textual.reactive import Reactive
+from textual.widget import Widget
 
 from lexing.lexer import Lexer
 
@@ -14,8 +18,33 @@ def run(file, lex):
     if lex:
         lexer = Lexer(source_code)
 
-        for token in lexer.lex():
-            click.echo(token)
+        tokens = list(lexer.lex())
+        _ptr = 0
+
+        class TokenDisplay(Widget):
+            def render(self) -> Panel:
+                token = tokens[PrettyLexer.token_pos]
+                highlighted_source = (
+                    "[white]"
+                    f"{source_code[:token.position[0]]}[red]"
+                    f"{source_code[token.position[0]:token.position[1] + 1]}[/red]{source_code[token.position[1] + 1:]}"
+                    "[/white]"
+                )
+                return Panel(highlighted_source)
+
+        class PrettyLexer(App):
+            token_pos = Reactive(0)
+
+            async def on_mount(self) -> None:
+                await self.view.dock(TokenDisplay(), edge="top")
+
+            def on_key(self, event):
+                if event.key == "left":
+                    self.token_pos -= 1 if self.token_pos != 0 else 0
+                elif event.key == "right":
+                    self.token_pos += 1 if self.token_pos != len(source_code) - 1 else 0
+
+        PrettyLexer.run(log="textual.log")
 
 
 if __name__ == "__main__":
